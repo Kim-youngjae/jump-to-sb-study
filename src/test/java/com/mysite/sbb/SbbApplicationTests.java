93c57e8ab5e1fbf996b7bec58f44bff30a31077b
youngjae459 <youngjae459@gmail.com>
2023-03-20 15:12:17 +0900

    question엔티티 테스트케이스 작성

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.answer.repository.AnswerRepository;
 import com.mysite.sbb.question.entity.Question;
 import com.mysite.sbb.question.repository.QuestionRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -12,11 +13,14 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
 	@DisplayName("DB에 데이터 추가")
@@ -48,10 +52,63 @@ class SbbApplicationTests {
 	@DisplayName("Question 엔티티 id값으로 데이터 조회")
 	void testJpa3() {
 		Optional<Question> oq = this.questionRepository.findById(1);
-		if(oq.isPresent()) {
+		if (oq.isPresent()) {
 			Question q = oq.get();
 			assertEquals("sbb가 무엇인가요?", q.getSubject());
 		}
 	}
 
+	@Test
+	@DisplayName("findBySubject")
+	void testJpa4() {
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void testJpa5() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
+	@Test
+	@DisplayName("findBySubjectLike")
+	void testJpa6() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%"); //sbb로 시작하는 모든 문자열
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("데이터 수정")
+	void testJpa7() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목"); // setter()로 제목을 변경 후
+		this.questionRepository.save(q); // 저장
+		Question q1 = this.questionRepository.findBySubject("수정된 제목");
+		assertEquals(1, q1.getId());
+		assertEquals("수정된 제목", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("데이터 삭제")
+	void testJpa8() {
+		// 데이터가 정상적으로 두 개가 들어가 있는지 확인하고
+		assertEquals(2, this.questionRepository.count()); // 해당 리포지터리의 총 데이터 건수를 리턴
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent()); // oq가 존재하면
+		Question q = oq.get(); //oq객체를 얻어와서
+		this.questionRepository.delete(q); // 삭제
+		assertEquals(1, this.questionRepository.count()); // 삭제하고 행이 2개에서 1개로 변경되었는지 확인
+	}
+
+	@Test
+	@DisplayName("답변 데이터 생성 후 저장하기")
+	void testJpa9() {
+
+	}
 }
